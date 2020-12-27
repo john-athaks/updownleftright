7d76b72442f30947fd263f25fbda7aae134d5a03
john-athaks <76246833+john-athaks@users.noreply.github.com>
2020-12-27 17:23:23 +0200

    προσθέσαμε μουσική σε "ελεύθερη πτώση" και "παρατεταμένη πίεση"

--- main.ts
+++ main.ts
@@ -1,12 +1,18 @@
 input.onGesture(Gesture.LogoUp, function () {
     basic.showIcon(IconNames.Square)
 })
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    music.playMelody("C5 A B G A F G E ", 120)
+})
 input.onGesture(Gesture.TiltLeft, function () {
     basic.showIcon(IconNames.SmallSquare)
 })
+input.onGesture(Gesture.FreeFall, function () {
+    music.playMelody("E B C5 A B G A F ", 120)
+})
 input.onGesture(Gesture.TiltRight, function () {
-    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
 })
 input.onGesture(Gesture.LogoDown, function () {
-    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
 })
