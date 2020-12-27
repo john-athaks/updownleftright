6fcc2e9a00aa4cc83769ba9fb24f216eb8e4645d
john-athaks <76246833+john-athaks@users.noreply.github.com>
2020-12-27 16:40:55 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Square)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Diamond)
+})
